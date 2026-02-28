7f723fef78d930bf86bb56293193d321fdc0c7de
filippozuppini <zuppini.filippo@gmail.com>
2026-02-28 13:27:35 +0100

    Add fix codition Fizzbuzz

--- main.js
+++ main.js
@@ -10,6 +10,8 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
 */
 
+
+
 //Stampo i numeri da 1 a 100
 
 for (let i = 1; i<=100; i++){
@@ -18,19 +20,18 @@ for (let i = 1; i<=100; i++){
 
     if(i%3==0){ //se multiplo di 3
         //Alora Stampo i + Fizz
-        console.log(i + 'Fizz');
+        console.log('Fizz');
 
     } else if (i%5==0){ //se multiplo di 5
         //Alora Stampo i + Buzz
-        console.log(i + 'Buzz');
+        console.log('Buzz');
 
-    } else if (i%3==0 && i%5==0){ // multiplo di 3 e 5
+    } else if (i%3==0 && i%5==0){ //se multiplo di 3 e 5
         // Allora Stampo i + FizzBuzz
-        console.log(i + 'FizzBuzz');
+        console.log('FizzBuzz');
         
     } else {
         console.log(i);
-        
     }
 
 }
