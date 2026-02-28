f4b6237280f3115f65609346303e6b745aaaf036
filippozuppini <zuppini.filippo@gmail.com>
2026-02-28 12:54:56 +0100

    Add logic

--- main.js
+++ main.js
@@ -10,5 +10,28 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
 */
 
+//Stampo i numeri da 1 a 100
 
+for (let i = 1; i<=100; i++){
+
+    // console.log(i);
+
+    if(i%3==0){ //se multiplo di 3
+        //Alora Stampo i + Fizz
+        console.log(i + 'Fizz');
+
+    } else if (i%5==0){ //se multiplo di 5
+        //Alora Stampo i + Buzz
+        console.log(i + 'Buzz');
+
+    } else if (i%3==0 && i%5==0){ // multiplo di 3 e 5
+        // Allora Stampo i + FizzBuzz
+        console.log(i + 'FizzBuzz');
+        
+    } else {
+        console.log(i);
+        
+    }
+
+}
 
